224b5864459088d5501ed7cba66d1a83e21db432
Pinsonn Laverdure <swegene@gmail.com>
2013-08-12 10:14:29 -0400

    lab3 part 2

---
+++ LABS/Ass 3/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Secret Log </title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv = "refresh" content = "5">
+	<link rel="stylesheet" type="text/css" href="style1.css" />
+	<script type="text/javascript" src="jquery.js"></script>
+		
+</head>
+
+<body>
+	<img id= "img1" src="Iron_man.jpg"  alt="ironman">
+		<div id="header">
+			<h1 class="text" > Here is my secret log<h1>
+		</div>
+
+		<div id="log">
+		</div>
+		
+	<div id="form">
+		
+		<form action="welcome.php" method="post">
+			<textarea name="content" rows="4" cols="50">
+			</textarea>
+			<input type="submit" value="Add entry">
+			<input type="submit" value="Reload page">
+		</form>
+	</div>
+<?php
+
+
+
+?>
+</body>
+</html>
