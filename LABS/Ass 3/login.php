85907b1b66bfe4977edb1c84b761462b2c2ec215
Andrew Webber <lolpower@hotmail.com>
2013-08-13 17:45:08 -0400

    Added more code, still doesn't do Password check though

--- LABS/Ass 3/login.php
+++ LABS/Ass 3/login.php
@@ -1,68 +1,84 @@
+<?php
+session_save_path("/www/home/a/a_webbe/s287/lab3/temp");
+session_start(); ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Lab 3 - login</title>
-	<link rel ="stylesheet" type="text/css" href="login.css" />
+        <title>Lab 3 - Login</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel ="stylesheet" type="text/css" href="login.css" />
 </head>
 <body>
 
-	<?php
+        <div id="login">
+                <h4>Login Credentials</h4>
+                <table>
+                <form method="post" action="" id="frm">
+                        <tr>
+                                <th>User:</th>
+                                <td><input type="text" name="xuser" /><td>
+                        </tr>
+                        <tr>
+                                <th>Password:</th>
+                                <td><input type="password" name="xpass" /><td>
+                        </tr>
+                </form>
+                </table>
+                <p name="err"></p>
+                <p id="button"><input name="submit" type="submit" value="Login" /></p>
+        </div>
 
-	$user = "";
-	if (isset($_REQUEST["user"])) {
-        	$user = $_REQUEST["user"];
-	}
+<?php
 
-	$pass = "";
-	if (isset($_REQUEST["pass"])) {
-		$pass = $_REQUEST["xpass"];
-	}
-	
-	$document = new DOMDocument();
-	$document->validateOnParse = true;
-	$document->load("login.xml");
+function logout() {
+        $user = "";
+        $pass = "";
+        $goodpass = "";
+}
 
-	$usertag = $document->getElementById($user);
-	$goodpass = "";
-	
-	if (isset($usertag)) {
-		foreach ($usertag->childNodes as $tag) {
-			if ($tag->nodeType == 1 && $tag->tagName == "password") {
-				$goodpass = $tag->nodevalue;
-			}
-		}
-	}
+function login() {
+        $user = "";
+        if (isset($_REQUEST["xuser"])) {
+                $user = $_REQUEST["xuser"];
+        }
 
-	if (sha1($user . $pass) == $goodpass) {
-		print "<p>Good Password</p>";
-		$_SESSION["username"] = $user;
-	} else {
-       		$errmsg = '<p id="err"></p>';
-		$doc = new DOMDocument();
-		$doc->loadHTML($errmsg);
-		$node->nodeValue = 'Incorrect Username / Password!';
-	        $_SESSION["username"] = "";
-	}
+        $pass = "";
+        if (isset($_REQUEST["xpass"])) {
+                $pass = $_REQUEST["xpass"];
+        }
 
-	?>
+        $document = new DOMDocument();
+        $document->validateOnParse = true;
+        $document->load("login.xml");
 
-	<div id="login">
-		<h4>Login Credentials</h4>
-		<form method="POST" action=? id="frm">
-		<table>
-			<tr>
-				<th>User:</th>
-				<td><input type="text" name="user" /><td>
-			</tr>
-			<tr>
-				<th>Password:</th>
-				<td><input type="password" name="pass" /><td>
-			</tr>
-		</table>
-		</form>
-		<p id="err"></p>
-		<p id="button"><input type="submit" value="Login" /></p>
-	</div>
+        $usertag = $document->getElementById($user);
+        $goodpass = "";
+
+        if (isset($usertag)) {
+                foreach ($usertag->childNodes as $tag) {
+                        if ($tag->nodeType == 1 && $tag->tagName == "password") {
+                                $goodpass = $tag->nodevalue;
+                        }
+                }
+        }
+
+        if (sha1($user . $pass) == $goodpass) {
+                print "<p>Good Password</p>";
+                $_SESSION["username"] = $user;
+        } else {
+                print "<p>Bad Password</p>";
+                $_SESSION["username"] = "";
+        }
+}
+
+if(isset($_POST['submit'])) {
+        login();
+}
+
+if(isset($_POST['logout'])) {
+        logout();
+}
+
+?>
 </body>
 </html>
